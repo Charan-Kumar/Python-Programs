489e22de9443abcf17bef2f2b41d09d265645954
Charan Kumar Borra <charan@crypsis.net>
2015-04-15 14:36:14 +0530

    Adds Program_03

---
+++ Program_03.py
@@ -0,0 +1,9 @@
+__author__ = 'charan'
+#3. Write a program to find largest of 2 numbers
+
+def large_number(num1,num2):
+    if num1 > num2 :
+        print("The Larger Number is ",num1)
+    else:
+        print("The Larger Number is ",num2)
+
