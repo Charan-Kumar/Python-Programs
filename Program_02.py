69d3a00b69b71c6b40bd18379cae99a95fbb38a5
Charan Kumar Borra <charan@crypsis.net>
2015-04-15 14:33:36 +0530

    Adds Program_02

---
+++ Program_02.py
@@ -0,0 +1,8 @@
+__author__ = 'charan'
+#2. Write a program to check a number is odd or even.
+
+def even_or_odd(num):
+    if(num%2):
+        print("Odd Number")
+    else:
+        print("Even Number")
